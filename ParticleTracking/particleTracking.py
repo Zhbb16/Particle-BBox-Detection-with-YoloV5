322e4964cdc44e512a84d548dca8b103a61dc6ae
Zahra Habibi <45474543+Zhbb16@users.noreply.github.com>
2024-03-16 22:14:03 -0400

    add new files

---
+++ ParticleTracking/particleTracking.py
@@ -0,0 +1,158 @@
+import cv2 as cv
+import argparse
+import sys
+import numpy as np
+import os.path
+import math
+from utils.centroidtracker import CentroidTracker
+from utils.trackableobject import TrackableObject
+
+# Initialize the parameters
+confThreshold = 0.6  #Confidence threshold
+nmsThreshold = 0.4   #Non-maximum suppression threshold
+inpWidth = 416       #Width of network's input image
+inpHeight = 416      #Height of network's input image
+
+parser = argparse.ArgumentParser(description='Object Detection using YOLO in OPENCV')
+
+parser.add_argument('--video', default='test.mp4', help='Path to video file.')
+args = parser.parse_args()
+        
+# Load names of classes
+classesFile = "coco.names"
+classes = None
+with open(classesFile, 'rt') as f:
+    classes = f.read().rstrip('\n').split('\n')
+
+# Give the configuration and weight files for the model and load the network using them.
+modelConfiguration = "yolov3.cfg"
+modelWeights = "yolov3.weights"
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_OPENCL)
+
+# initialize the video writer
+writer = None
+ 
+# initialize the frame dimensions (we'll set them as soon as we read
+# the first frame from the video)
+W = None
+H = None
+ 
+# instantiate our centroid tracker, then initialize a list to store
+# each of our dlib correlation trackers, followed by a dictionary to
+# map each unique object ID to a TrackableObject
+ct = CentroidTracker(maxDisappeared=40, maxDistance=50)
+trackers = []
+trackableObjects = {}
+ 
+# initialize the total number of frames processed thus far, along
+# with the total number of objects that have moved either up or down
+totalDown = 0
+totalUp = 0
+
+# Get the names of the output layers
+def getOutputsNames(net):
+    # Get the names of all the layers in the network
+    layersNames = net.getLayerNames()
+    # Get the names of the output layers, i.e. the layers with unconnected outputs
+    return [layersNames[i - 1] for i in net.getUnconnectedOutLayers()]
+
+
+# Remove the bounding boxes with low confidence using non-maxima suppression
+def postprocess(frame, outs):
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+
+    rects = []
+
+    # Scan through all the bounding boxes output from the network and keep only the
+    # ones with high confidence scores. Assign the box's class label as the class with the highest score.
+    classIds = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                center_x = int(detection[0] * frameWidth)
+                center_y = int(detection[1] * frameHeight)
+                width = int(detection[2] * frameWidth)
+                height = int(detection[3] * frameHeight)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                classIds.append(classId)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+    # Perform non maximum suppression to eliminate redundant overlapping boxes with
+    # lower confidences.
+    indices = cv.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+    for i in indices:
+        box = boxes[i]
+        left = box[0]
+        top = box[1]
+        width = box[2]
+        height = box[3]
+        if classIds[i] == 0:
+            rects.append((left, top, left + width, top + height))
+            # use the centroid tracker to associate the (1) old object
+            # centroids with (2) the newly computed object centroids
+            objects = ct.update(rects)
+
+# Process inputs
+winName = 'People Counting and Tracking System'
+cv.namedWindow(winName, cv.WINDOW_NORMAL)
+
+outputFile = "yolo_out_py.avi"
+
+if (args.video):
+    # Open the video file
+    if not os.path.isfile(args.video):
+        print("Input video file ", args.video, " doesn't exist")
+        sys.exit(1)
+    cap = cv.VideoCapture(args.video)
+    outputFile = args.video[:-4]+'_output.avi'
+else:
+    # Webcam input
+    cap = cv.VideoCapture(0)
+
+# Get the video writer initialized to save the output video
+vid_writer = cv.VideoWriter(outputFile, cv.VideoWriter_fourcc('M','J','P','G'), 30, (round(cap.get(cv.CAP_PROP_FRAME_WIDTH)),round(cap.get(cv.CAP_PROP_FRAME_HEIGHT))))
+
+while cv.waitKey(1) < 0:
+    
+    # get frame from the video
+    hasFrame, frame = cap.read()
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+    cv.line(frame, (0, frameHeight // 2), (frameWidth, frameHeight // 2), (0, 255, 255), 2)
+    
+    # Stop the program if reached end of video
+    if not hasFrame:
+        print("Done processing !!!")
+        print("Output file is stored as ", outputFile)
+        cv.waitKey(3000)
+        # Release device
+        cap.release()
+        break
+
+    # Create a 4D blob from a frame.
+    blob = cv.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
+
+    # Sets the input to the network
+    net.setInput(blob)
+
+    # Runs the forward pass to get output of the output layers
+    outs = net.forward(getOutputsNames(net))
+
+    # Remove the bounding boxes with low confidence
+    postprocess(frame, outs)
+
+
+    
